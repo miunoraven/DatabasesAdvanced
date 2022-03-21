1919440772d2092f7d608cd5f640104ba19a0509
Miuno Raven <r0797856@student.thomasmore.be>
2022-03-21 22:54:48 +0100

    Update parcer.py

--- Redis/parcer.py
+++ Redis/parcer.py
@@ -4,16 +4,16 @@ import pymongo as mongo
 while True:
     r = redis.Redis(host='localhost', port=6379, db=0)
 
-    client = mongo.MongoClient("mongodb ://127.0.0.1:27017")
+    client = mongo.MongoClient("mongodb://127.0.0.1:27017")
 
     bitcoins_db = client["bitcoins"]
     topten = bitcoins_db["bitcoins_topten"]
 
-    for key in client.scan_iter():
-        hash = {    "Hash": client.hmget(key, "hash"),
-                    "Time": client.hmget(key, "time"),
-                    "USD": client.hmget(key, "usd"),
-                    "BTC": client.hmget(key, "btc")
+    for key in r.scan_iter():
+        hash = {    "Hash": r.hmget(key, "hash")[0],
+                    "Time": r.hmget(key, "time")[0],
+                    "USD": r.hmget(key, "usd")[0],
+                    "BTC": r.hmget(key, "btc")[0]
         }
         x = topten.insert_one(hash)
         r.delete(key)
